eb45aa28b1b1092f84e9b8fd32010f309d8bc825
Victor <victor.beasse6@gmail.com>
2026-08-29 15:36:19 +0200

    Texture class implementation

--- include/noctis_engine/rendering/texture.hpp
+++ include/noctis_engine/rendering/texture.hpp
@@ -12,85 +12,85 @@
 namespace NoctisEngine::Rendering
 {
 
-/// @brief Describes a texture's data, do not use this directly, 
-/// except if you know what you're doing, instead use Asset::load_texture
-struct TextureData 
+/// @brief The function used when shrinking the texture
+enum class MinifyingFunction : std::uint32_t
 {
-    /// @brief The texture's pixel data
-    uint8_t *data;
+    /// @brief Picks the closest pixel, best for pixel art rendering
+    NEAREST = 0x2600,
 
-    /// @brief The texture's width
-    int width; 
+    /// @brief Blends the 4 nearest pixels, smooth but not mipmaps
+    LINEAR = 0x2601,
 
-    /// @brief The texture's height
-    int height;
-    /// @brief The texture's number of color channels
-    int nr_channels;
+    /// @brief Picks the closest mip level, then chooses the nearest pixel
+    NEAREST_MIPMAP_NEAREST = 0x2700,
 
-    /// @brief The texture's name
-    std::string name;
+    /// @brief Blends between the two closest mip levels, then chooses the nearest pixel
+    LINEAR_MIPMAP_NEAREST = 0x2701,
+    
+    /// @brief Picks the closest mip level, then does linear filtering
+    NEAREST_MIPMAP_LINEAR = 0x2702,
+
+    /// @brief Blends between the two closest mip levels, then does linear filtering
+    LINEAR_MIPMAP_LINEAR = 0x2703,
+};
+
+/// @brief The function used when magnifying the texture
+enum class MagnifyingFunction : std::uint32_t 
+{
+    /// @brief Picks the closest pixel, best for pixel art rendering
+    NEAREST = 0x2600,
+
+    /// @brief Blends the 4 nearest pixels, smoother but low quality-esque
+    LINEAR = 0x2601,
+};
+
+/// @brief This constrols what happens when texture coordinates go outside the [0, 1] range 
+enum class WrapParam : std::uint32_t 
+{
+    /// @brief Just repeat the edge pixel forever, stretching the texture
+    CLAMP_TO_EDGE = 0x812F,
+    
+    /// @brief Uses a defined border color instead of any pixel
+    CLAMP_TO_BORDER = 0x812D,
+
+    /// @brief The texture repeats but flips each time it tiles
+    MIRRORED_REPEAT = 0x8370,
+
+    /// @brief the texture repeats over and over
+    REPEAT = 0x2901,
+
+    /// @brief Mirrors the texture once, then clamps to the edge beyond that
+    MIRROR_CLAMP_TO_EDGE = 0x8743,
 };
 
 /// @brief This class represents a texture stored on the GPU
 class Texture
 {
 public:
-    /// @brief The function used when shrinking the texture
-    enum class MinifyingFunction : std::uint32_t
+    /// @brief Describes a texture's data, do not use this directly, 
+    /// except if you know what you're doing, instead use Asset::load_texture
+    struct Data 
     {
-        /// @brief Picks the closest pixel, best for pixel art rendering
-        NEAREST = 0x2600,
+        /// @brief The texture's pixel data
+        uint8_t *data;
 
-        /// @brief Blends the 4 nearest pixels, smooth but not mipmaps
-        LINEAR = 0x2601,
+        /// @brief The texture's width
+        int width; 
 
-        /// @brief Picks the closest mip level, then chooses the nearest pixel
-        NEAREST_MIPMAP_NEAREST = 0x2700,
+        /// @brief The texture's height
+        int height;
+        /// @brief The texture's number of color channels
+        int nr_channels;
 
-        /// @brief Blends between the two closest mip levels, then chooses the nearest pixel
-        LINEAR_MIPMAP_NEAREST = 0x2701,
-        
-        /// @brief Picks the closest mip level, then does linear filtering
-        NEAREST_MIPMAP_LINEAR = 0x2702,
-
-        /// @brief Blends between the two closest mip levels, then does linear filtering
-        LINEAR_MIPMAP_LINEAR = 0x2703,
-    };
-
-    /// @brief The function used when magnifying the texture
-    enum class MagnifyingFunction : std::uint32_t 
-    {
-        /// @brief Picks the closest pixel, best for pixel art rendering
-        NEAREST = 0x2600,
-
-        /// @brief Blends the 4 nearest pixels, smoother but low quality-esque
-        LINEAR = 0x2601,
-    };
-
-    /// @brief This constrols what happens when texture coordinates go outside the [0, 1] range 
-    enum class WrapParam : std::uint32_t 
-    {
-        /// @brief Just repeat the edge pixel forever, stretching the texture
-        CLAMP_TO_EDGE = 0x812F,
-        
-        /// @brief Uses a defined border color instead of any pixel
-        CLAMP_TO_BORDER = 0x812D,
-
-        /// @brief The texture repeats but flips each time it tiles
-        MIRRORED_REPEAT = 0x8370,
-
-        /// @brief the texture repeats over and over
-        REPEAT = 0x2901,
-
-        /// @brief Mirrors the texture once, then clamps to the edge beyond that
-        MIRROR_CLAMP_TO_EDGE = 0x8743,
+        /// @brief The texture's name
+        std::string name;
     };
 
     /// @brief Creates a texture and uploads its data to the GPU
     /// @param texture_data The texture's data
-    Texture(const TextureData &texture_data);
+    Texture(const Data &texture_data);
 
-    ~Texture();
+    ~Texture() = default;
 
     /// @brief Sets the texture's minifying function
     /// @param func The minifying function 
@@ -98,7 +98,7 @@ public:
 
     /// @brief Sets the texture's magnifying function
     /// @param func The magnifying function 
-    auto set_magnifying_function(MinifyingFunction func) const -> void;
+    auto set_magnifying_function(MagnifyingFunction func) const -> void;
 
     /// @brief Sets the texture's wrap parameters
     /// @param u Horizontal wrap parameter
@@ -109,6 +109,11 @@ public:
     /// @important Only useful if you use the CLAMP_TO_BORDER wrap parameter
     /// @param c The texture's border color
     auto set_border_color(const Color &c) const -> void;
+
+    /// @brief Sets this texture to be bound in a draw list
+    /// @param draw_list The draw list
+    /// @param bind_point The point the texture should be bound to in the shader
+    auto bind(DrawList &draw_list, std::uint32_t bind_point) const -> void;
 
     /// @brief Removes this texture from the GPU, it shouldn't be used after deletion
     auto delete_gpu() const -> void;
@@ -126,10 +131,11 @@ public:
     auto name() const -> std::string;
 
 private:
-    std::uint32_t handle_;
+    std::uint32_t   handle_;
+    std::string     name_;
 
-    int width_;
-    int height_;
+    int             width_;
+    int             height_;
 };
 
 namespace TextureSlots
